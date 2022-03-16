5e0d46f3f371d9adc5a75f780f39e50a1c459337
Muhiddinov Sherzamon <98773671+Sherzamon20010206@users.noreply.github.com>
2022-03-16 21:11:07 +0500

    Update settings.py

--- Homework/settings.py
+++ Homework/settings.py
@@ -79,14 +79,23 @@ WSGI_APPLICATION = 'Homework.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'sherzamon',
+        'USER': 'sherzamon',
+        'PASSWORD': 'sherzamon',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
